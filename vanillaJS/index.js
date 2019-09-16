2a2bba4066ecdcb01a13d5fa6b6a6f05b0398f5a
H-web <44045307+H-web@users.noreply.github.com>
2019-09-16 23:03:40 +0900

    Add files via upload

---
+++ vanillaJS/index.js
@@ -0,0 +1,11 @@
+console.log("Hi I'm Vanilla JS");
+
+var a = 221;
+var b = a - 5;
+
+function pig() {
+  confirm("몸무게를 줄이고 싶습니까?", "");
+  if ((prompt = true)) {
+    alert("그런 방법은 없습니다... 밥그릇만 보세요");
+  }
+}
